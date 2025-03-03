16a4f1e2415ba04664c63746e7ada7a1bbb94056
iPeterNL <49556994+iPeterNL@users.noreply.github.com>
2025-03-04 00:52:54 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@ self.addEventListener('install', function(event) {
                 '/',
                 '/index.html',
                 '/manifest.json',
-                '/icon.png'
+                '/icon.jpg'
             ]);
         })
     );
